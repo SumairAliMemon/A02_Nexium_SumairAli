7b378f3f179965780db12b0d62dd3eeccad47c3d
SumairAliMemon <sumairmemon897@gmail.com>
2025-07-14 01:31:36 +0500

    final changes

--- src/app/api/test-multiple/route.ts
+++
@@ -1,9 +0,0 @@
-
-// Test URLs that should work
-const TEST_URLS = [
-  'https://httpbin.org/html',
-  'https://example.com',
-  'https://jsonplaceholder.typicode.com/posts/1',
-  'https://www.google.com',
-];
-
